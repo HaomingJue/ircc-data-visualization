d6730349fd47310b052e0d4761c38b6ca9e688b3
haomingjue <haomingjue@outlook.com>
2022-10-30 13:53:24 -0400

    fix: fixed floc error by changing browserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from "./pages/login/login.page";
 import { RegisterPage } from "./pages/register/register.page";
 import { HomePage } from "./pages/home/home.page";
@@ -8,13 +8,13 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter >
+    <HashRouter >
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
