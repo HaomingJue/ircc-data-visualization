1bf3fa17a7dc2f50980a9bf26d676c31b1587ad3
jkL970910 <jkliu97@gmail.com>
2022-11-28 00:21:17 -0500

    line chart implement

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,15 +1,78 @@
+import {useState, useEffect} from 'react';
 import { ResponsiveLine } from "@nivo/line";
 import { useTheme } from "@mui/material";
 import { tokens } from "../common/theme";
-import { mockLineData as data } from "../mockData/mockData";
+import { checkLoginStatus } from "../service/checkLoginStatus";
+import { useNavigate } from "react-router-dom";
+import { handleRequest, HttpRequest } from '../model/http_request';
 
 const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+    const [lineData, setLineData] = useState([]);
+    const [yMax, setYMax] = useState(400000);
+
+    let navigate = useNavigate();
+
+    useEffect( () => {
+      if (!checkLoginStatus()) {
+        navigate("/login");
+      }
+      getCategoryData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[navigate])
+
+    const getCategoryData = () => {
+      let request = new HttpRequest('Get', `/datasource/category/*`);
+      handleRequest(request).then((a) => dataConstruct(a.data)).catch((err) => alert(err));
+    }
+
+    const dataConstruct = (datas) => {
+      datas.sort((data1, data2) => data1.year - data2.year)
+      //setYMax(Math.max(datas.map()))
+      const lineData = [];
+      for (let key in datas[0]) {
+        if (key !== 'id' && key !== 'year') {
+          lineData.push({id: key, color: tokens("dark").greenAccent[500], data: []});
+        }
+      }
+      for (let index in datas) {
+        let data = datas[index]
+        lineData.find(obj => obj.id === 'federal_skilled_worker')?.data.push({
+          x: data['year'],
+          y: data['federal_skilled_worker'],
+        });
+        lineData.find(obj => obj.id === 'quebec_skilled_worker')?.data.push({
+          x: data['year'],
+          y: data['quebec_skilled_worker'],
+        });
+        lineData.find(obj => obj.id === 'provincial_nominee_program')?.data.push({
+          x: data['year'],
+          y: data['provincial_nominee_program'],
+        });
+        lineData.find(obj => obj.id === 'family_sponsorship')?.data.push({
+          x: data['year'],
+          y: data['family_sponsorship'],
+        });
+        lineData.find(obj => obj.id === 'business_immigrant')?.data.push({
+          x: data['year'],
+          y: data['business_immigrant'],
+        });
+        lineData.find(obj => obj.id === 'total')?.data.push({
+          x: data['year'],
+          y: data['total'],
+        });
+        lineData.find(obj => obj.id === 'canadian_experience_class')?.data.push({
+          x: data['year'],
+          y: data['canadian_experience_class'],
+        });
+      }
+      setLineData(lineData);
+    }
   
     return (
       <ResponsiveLine
-        data={data}
+        data={lineData}
         theme={{
           axis: {
             domain: {
@@ -44,13 +107,12 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
           },
         }}
         colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }} // added
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        margin={{ top: 50, right: 150, bottom: 50, left: 100 }}
         xScale={{ type: "point" }}
         yScale={{
           type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: true,
+          min: 'auto',
+          max: 'auto',
           reverse: false,
         }}
         yFormat=" >-.2f"
@@ -62,20 +124,19 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
           tickSize: 0,
           tickPadding: 5,
           tickRotation: 0,
-          legend: isDashboard ? undefined : "transportation", // added
+          legend:  "Year", // added
           legendOffset: 36,
           legendPosition: "middle",
         }}
         axisLeft={{
-          orient: "left",
-          tickValues: 5, // added
-          tickSize: 3,
+          orient: 'left',
+          tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: isDashboard ? undefined : "count", // added
-          legendOffset: -40,
-          legendPosition: "middle",
-        }}
+          legend: 'Number of Immigrants',
+          legendOffset: -70,
+          legendPosition: 'middle'
+      }}
         enableGridX={false}
         enableGridY={false}
         pointSize={8}
--- src/pages/home/line-chart/line-chart.jsx
+++ src/pages/home/line-chart/line-chart.jsx
@@ -5,7 +5,7 @@ import LineChart from "../../../components/LineChart";
 const LineChartPage = () => {
   return (
     <Box m="20px">
-      <Header title="Line Chart" subtitle="Simple Line Chart" />
+      <Header title="Yearly Trends" subtitle="Check Yearly Trends of Canadian Immigrations" />
       <Box height="75vh">
         <LineChart />
       </Box>
