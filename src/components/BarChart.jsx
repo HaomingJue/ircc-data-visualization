c60c92bd6ce84c88849a70d62d9abb247ba5ef0b
haomingjue <haomingjue@outlook.com>
2022-11-27 19:31:14 -0500

    adjusted gap

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -80,7 +80,7 @@ const BarChart = ({ isDashboard = false }) => {
             "quebec_skilled_worker",
             "federal_skilled_worker"]}
       indexBy="province"
-      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      margin={{ top: 50, right: 200, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
       indexScale={{ type: "band", round: true }}
