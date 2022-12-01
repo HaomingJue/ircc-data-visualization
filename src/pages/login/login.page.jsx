df9901c21bb6a4839690258caab827588fe03f4d
jkL970910 <jkliu97@gmail.com>
2022-11-30 21:41:22 -0500

    Revert "Merge pull request #66 from HaomingJue/jiekunl/validate-code"
    This reverts commit 375e9e86a1c87628729ae395fd4f18f70636e93b, reversing
    changes made to e0f034d8145ca9f219eecd5f99ced8a80be800ab.

--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -20,15 +22,14 @@ import { useTheme } from '@emotion/react';
 import { useEffect } from 'react';
 import {  setLocal } from '../../service/localStorage';
 import { checkLoginStatus } from '../../service/checkLoginStatus';
-import Captcha from 'react-captcha-code';
 
 export function LoginPage() {
   
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const [code, setCode] = React.useState();
 
   let navigate = useNavigate(); 
+  // default check valid token
 
   useEffect(() => {
     if (checkLoginStatus()) {
@@ -55,12 +56,8 @@ export function LoginPage() {
     let loginRequest = {};
     loginRequest["username"] = data.get('username');
     loginRequest["password"] = data.get('password');
-    if (data.get('validateCode') !== code) {
-      alert("Please enter correct validate code!")
-    } else {
-      let request = new HttpRequest('Post', '/login/', loginRequest);
-      handleRequest(request).then((a) => handleLogin(a)).catch((err) => handleLogin(err));
-    }
+    let request = new HttpRequest('Post', '/login/', loginRequest);
+    handleRequest(request).then((a) => handleLogin(a)).catch((err) => handleLogin(err));
   };
 
   return (
@@ -102,21 +99,10 @@ export function LoginPage() {
               id="password"
               autoComplete="current-password"
             />
-            <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '24px'}}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="validateCode"
-                label="Validate"
-                name="validateCode"
-                autoComplete="Validate"
-                autoFocus
-              />
-              <Box sx={{ mt: 2}} >
-                <Captcha height={56} charNum={4} onChange={(e) => {setCode(e)}} />
-              </Box>
-            </Box>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
             <Button
               type="submit"
               fullWidth
@@ -125,7 +111,12 @@ export function LoginPage() {
             >
               Sign In
             </Button>
-            <Grid container sx={{justifyContent: 'center'}}>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2" color={colors.primary[100]}>
+                  Forgot password?
+                </Link>
+              </Grid>
               <Grid item>
                 <Link href="/register" variant="body2" color={colors.primary[100]}>
                   {"Don't have an account? Sign Up"}
